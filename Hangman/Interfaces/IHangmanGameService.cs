e0e54a4ec5f4dc45f3118c9b72aa3a256839714f
basileia <jane.novak@centrum.cz>
2025-10-15 12:50:57 +0200

    Updated Home.razor to inject IGameFactory for game instance creation

--- Hangman/Interfaces/IHangmanGameService.cs
+++ Hangman/Interfaces/IHangmanGameService.cs
@@ -2,6 +2,11 @@
 {
     public interface IHangmanGameService
     {
+        string SecretWord { get; }
+        HashSet<char> GuessedLetters { get; }
+        int NumberOfMistakes { get; }
+        int MaxMistakes { get; }
+
         (bool Success, string Message) Guess(char letter);
         string GetCurrentProgress();
         bool IsGameOver();
